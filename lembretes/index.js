ebe601382365c0816705a49754954ca548ad1ba9
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2023-04-29 09:45:58 -0300

    apostila03(mss): fim da secao 4.3.14

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,15 +1,29 @@
-// POST localhost:4000/lembretes {texto: "fazer café"}
-// GET localhost:4000/lembretes
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
+app.use(express.json());
+/*
 
-app.post('/lembretes', (req, res) => {
-    
+{
+    "1": {id:"1", texto: "fazer café"},
+    "2": {id:"2", texto: "comprar frutas"},
+}
+
+*/
+let id = 0;
+const lembretes = {};
+
+//POST localhost:4000/lembretes {texto: "fazer café"}
+app.post("/lembretes", (req, res) => {
+  const texto = req.body.texto;
+  id++;
+  lembretes[id] = { id, texto };
+  res.status(201).json(lembretes[id]);
+});
+// GET localhost:4000/lembretes
+app.get("/lembretes", (req, res) => {
+  res.send(lembretes);
 });
 
-app.get('/lembretes', (req, res) => {
-    
-});
-
-const { PORT } = process.env;
-app.listen(PORT || 4000 , () => console.log(`Lembretes. Porta: ${PORT}`))
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Lembretes. Porta: ${PORT}`));
